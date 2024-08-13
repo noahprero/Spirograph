fbde67401e22833e1390fda5c85326ceb7c8133b
noahprero <131603418+noahprero@users.noreply.github.com>
2024-08-13 11:19:55 -0400

    Fixed visual bugs

--- spirograph.js
+++ spirograph.js
@@ -160,8 +160,8 @@ function initialize_circle_array() {
     circles.push(base_circle);
 
     for(let i = 1; i < circle_count; i ++) {
-        circles.push(new Circle(ctx, 1, 1, 1, 0, (Math.random() * 2 - 1) * speed_multiplier));
-        //circles.push(new Circle(ctx, 1, 1, 1, 0, Math.pow(-4, i) / 1000));  // Fractal formula
+        //circles.push(new Circle(ctx, 1, 1, 1, 0, (Math.random() * 2 - 1) * speed_multiplier));
+        circles.push(new Circle(ctx, 1, 1, 1, 0, Math.pow(-4, i) / 100));  // Fractal formula
     }
     return base_circle;
 }
@@ -175,7 +175,8 @@ let draw_rotations = visual_toggle_rotations.checked;
 let circles = [];
 let circle_count = circle_count_input.value;
 let size_change = size_change_slider.value;  // Size of each circle relative to the previous circle
-let speed_multiplier = 0.1  // Value to multiply the initial random speed by
+let speed_multiplier = 0.06  // Value to multiply the initial random speed by
+let rotation_offset = 4;  // Value to rotation_offset the rotation's innaccuracy to the circle's center
 
 // Keep track of the previous tip circle position to draw lines making up the trail
 let prev_tip_x = 0;
@@ -210,9 +211,10 @@ function animate() {
         }
 
         if(draw_rotations) {
+            
             rotation_ctx.beginPath();
-            rotation_ctx.moveTo(circles[i-1].x, circles[i-1].y);
-            rotation_ctx.lineTo(x, y);
+            rotation_ctx.moveTo(circles[i-1].x + rotation_offset, circles[i-1].y + rotation_offset);
+            rotation_ctx.lineTo(x + rotation_offset, y + rotation_offset);
             rotation_ctx.stroke();
         }
         
